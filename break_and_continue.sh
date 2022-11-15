e46dd9dbf266335a3fd6a82e3fad2ec20e0587a9
Riyaz.b <riyaz.b@bet21onlineltd.onmicrosoft.com>
2022-11-15 16:29:43 +0530

    updated

--- break_and_continue.sh
+++ break_and_continue.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# Break
+for (( i=1; i<=5; i++ ))
+do
+    if [ $i -gt 5 ]
+    then 
+        break
+    fi
+    echo $i
+done
+
+for (( i=1; i<=10; i++ ))
+do 
+    if [ $i -eq 3 -o $i -eq 6 ]
+    then
+        continue
+    fi
+    echo "$i"
+done 
